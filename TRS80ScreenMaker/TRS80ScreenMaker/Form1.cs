1704a150c2f44f31570437850960634bcb63c7e4
evancwright <evancwright@yahoo.com>
2018-01-02 20:42:09 -0600

    TRS-80 screen data maker
    TRS-80 screen data maker

---
+++ TRS80ScreenMaker/TRS80ScreenMaker/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TRS80ScreenMaker
+{
+
+
+    public partial class Form1 : Form
+    {
+        const int WIDTH_SCALE = 10;
+        const int HEIGHT_SCALE = 20;
+        char[,] screen = new char[64, 24];
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            pictureBox1.Width = 64 * WIDTH_SCALE;
+            pictureBox1.Height = 64 * HEIGHT_SCALE;
+            Init();
+        }
+
+        void Init()
+        {
+            for (int i = 0; i < 64; i++)
+            {
+                for (int j = 0; j < 16; j++)
+                {
+                    screen[i, j] = ' ';
+                }
+            }
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            Brush black = Brushes.Black;
+            Brush white = Brushes.White;
+
+            for (int i = 0; i < 64; i++)
+            {
+                for (int j = 0; j < 16; j++)
+                {
+                    if (screen[i, j] == ' ')
+                    {
+                        e.Graphics.FillRectangle(black, i * WIDTH_SCALE, j * HEIGHT_SCALE, WIDTH_SCALE, HEIGHT_SCALE);
+                    }
+                    else
+                    {
+                        e.Graphics.FillRectangle(white, i * WIDTH_SCALE, j * HEIGHT_SCALE, WIDTH_SCALE, HEIGHT_SCALE);
+                    }
+                }
+            }
+
+        }
+
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            int x = e.X / WIDTH_SCALE;
+            int y = e.Y / HEIGHT_SCALE;
+
+            if (screen[x,y]==' ')
+                screen[x,y] = 'X';
+            else
+                screen[x,y] = ' ';
+
+            pictureBox1.Invalidate();
+        }
+
+        private void toASMToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            CodeForm cf = new CodeForm();
+            cf.Data = screen;
+            cf.ShowDialog();
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("By Evan Wright, 2018");
+        }
+    }
+}
---
+++ TRS80ScreenMaker/TRS80ScreenMaker/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("TRS80ScreenMaker")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TRS80ScreenMaker")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("34d50e8c-7a8a-4928-87af-8efffa10508e")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
